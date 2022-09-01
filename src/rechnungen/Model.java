b767dc71360b6301eebc500846e66f693d2c1827
Philipp <--->
2022-09-01 20:18:53 +0200

    Funktion für String zerlegen nicht fertig

--- src/rechnungen/Model.java
+++ src/rechnungen/Model.java
@@ -3,21 +3,23 @@ package rechnungen;
 public class Model {
 
 	public void berechne(String inputIP, String inputHost) {
-		
-		String ipAdresse;
-		String hostAnzahl;
-				
-		System.out.println("Ausgabe in berechne(): " + inputIP);
-		System.out.println("Ausgabe in berechne(): " + inputHost);
 
 		
-		String ipBinaer =ipInBinaer(inputIP);
-		System.out.println("Ausgabe in berechne ipBinaer" + inputIP);
-		System.out.println("Hier sollte Theoretisch >Test< angezeigt werden da es das Ergebnis der Umrechnung wäre :" + ipBinaer);
+		System.out.println("Ausgabe in berechne(): " + inputIP);
+		//System.out.println("Ausgabe in berechne(): " + inputHost);
+		String ip ="192.168.0.1";
+		//String split[] = StringUtils.split(ip, ".";)
+		//String[] arrOfIp = ip.split(".");
+		//for (String ausgabe : arrOfIp)
+			//System.out.println("Ausgabe nach dem String geteilt wurde " + ausgabe);
+		
+		//String ipBinaerErgebnis = ipInBinaer(inputIP);
+		//System.out.println("Ausgabe in berechne ipBinaer" + inputIP);
+		//System.out.println("Hier sollte Theoretisch >Test< angezeigt werden da es das Ergebnis der Umrechnung wäre :"
+			//	+ ipBinaerErgebnis);
 	}
 
-//Berechnung IP in Binaer
-
+	// Berechnung IP in Binaer
 	private String ipInBinaer(String ipAdresse) {
 
 		System.out.println("Ausgabe in ipInBinaer" + ipAdresse);
